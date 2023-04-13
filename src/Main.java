155cfa641868618a12bba453e080069d7502ecea
vaceslavkameronov <slka@mail.ru>
2023-04-14 00:34:40 +0400

    решение ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EmployeeManagement employeeManagement = new EmployeeManagement();
+        employeeManagement.addEmployee("Иванов", "Иван", "Иванович", 1, 11_000);
+        employeeManagement.addEmployee("Петров", "Петр", "Петрович", 2, 15000);
+        employeeManagement.addEmployee("Сидоров", "Сидор", "Сидорович", 3, 13000);
+        employeeManagement.person();
     }
 }
